c5496da93aa2bb886993aa180d275456fb7a4633
zainbhaila <zainbhaila@gmail.com>
2019-11-06 11:47:30 -0500

    starting p1

---
+++ MeeshQuest-Part1/src/cmsc420/meeshquest/part1/City.java
@@ -0,0 +1,35 @@
+package cmsc420.meeshquest.part1;
+
+public class City implements Comparable<City> {
+	public String name;
+	public String x;
+	public String y;
+	public String color;
+	public String radius;
+	
+	City(String name, String x, String y, String color, String radius) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.radius = radius;
+	}
+
+	@Override
+	public int compareTo(City arg0) {
+		return this.name.compareTo(((City) arg0).name);
+	}
+
+	public int compareToCoords(City arg0) {
+		String x1 = this.x;
+		String y1 = this.y;
+		String x2 = arg0.x;
+		String y2 = arg0.y;
+		if (Float.parseFloat(x1) == Float.parseFloat(x2)) {
+			return Float.compare(Float.parseFloat(y1), Float.parseFloat(y2));
+		}
+		else {
+			return Float.compare(Float.parseFloat(x1), Float.parseFloat(x2));
+		}
+	}
+}
---
+++ MeeshQuest-Part1/src/cmsc420/meeshquest/part1/MeeshQuest.java
@@ -0,0 +1,331 @@
+package cmsc420.meeshquest.part1;
+
+import java.io.FileInputStream;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import cmsc420.xml.XmlUtility;
+
+/**
+ * Skeleton implementation of MeeshQuest, Part 1, CMSC 420-0201, Fall 2019.
+ * This does the following: (1) opens the input/output files;
+ * (2) validates and parses the xml input;
+ * (3) iterates through the input command nodes, but doesn't do anything
+ * (4) prints the results.
+ */
+public class MeeshQuest {
+
+// --------------------------------------------------------------------------------------------
+//  Uncomment these to read from standard input and output (USE THESE FOR YOUR FINAL SUBMISSION)
+//	private static final boolean USE_STD_IO = true; 
+//	private static String inputFileName = "";
+//	private static String outputFileName = "";
+// --------------------------------------------------------------------------------------------
+//  Uncomment these to read from a file (USE THESE FOR YOUR TESTING ONLY)
+	private static final boolean USE_STD_IO = false;
+	private static String inputFileName = "test/mytest-input-2.xml";
+	private static String outputFileName = "test/mytest-output.xml";
+// --------------------------------------------------------------------------------------------
+
+	public static void main(String[] args) {
+
+		// configure to read from file rather than standard input/output
+		if (!USE_STD_IO) {
+			try {
+				System.setIn(new FileInputStream(inputFileName));
+				System.setOut(new PrintStream(outputFileName));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// results will be stored here
+		Document results = null;
+		try {
+			// generate XML document for results
+			results = XmlUtility.getDocumentBuilder().newDocument();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		ArrayList<City> clist = new ArrayList<City>();
+		BinarySearchTree<City> names = new BinarySearchTree<City>(); 
+		//TODO: add sg tree
+
+		try {
+			// validate and parse XML input
+			Document input = XmlUtility.validateNoNamespace(System.in);
+			// create results node
+			Element root = results.createElement("results");
+			results.appendChild(root);
+			// get input document root node
+			Element rootNode = input.getDocumentElement();
+			// get list of all nodes in document
+			final NodeList nl = rootNode.getChildNodes();
+			for (int i = 0; i < nl.getLength(); i++) {
+				// process only commands (ignore comments)
+				if (nl.item(i).getNodeType() == Document.ELEMENT_NODE) {
+					// get next command to process
+					Element command = (Element) nl.item(i); // (ignore warning - just a skeleton)if (command.getNodeName().equals("createCity")) {
+
+					if (command.getNodeName().equals("createCity")) {
+						// parse input command
+						String name = command.getAttribute("name"); 
+						String x = command.getAttribute("x"); 
+						String y = command.getAttribute("y"); 
+						String radius = command.getAttribute("radius");
+						String color = command.getAttribute("color"); 
+						
+						// check if duplicate names and records exist
+						boolean dupName = false;
+						boolean dupCoord = false;
+						for (City cit : clist) {
+							if (cit.name.compareTo(name) == 0) {
+								dupName = true;
+							}
+							if (cit.x.compareTo(x) == 0 && cit.y.compareTo(y) == 0) {
+								dupCoord = true;
+								break;
+							}
+						}
+						
+						if (dupCoord || dupName) { // duplicate name or coords
+							Element error = results.createElement("error");
+							root.appendChild(error);
+							error.setAttribute("type", dupCoord ? "duplicateCityCoordinates" : "duplicateCityName");
+							Element comm = results.createElement("command");
+							error.appendChild(comm);
+							comm.setAttribute("name", command.getNodeName());
+							Element parameters = results.createElement("parameters");
+							error.appendChild(parameters);
+							Element n = results.createElement("name");
+							n.setAttribute("value", name);
+							parameters.appendChild(n);
+							Element xc = results.createElement("x");
+							xc.setAttribute("value", x);
+							parameters.appendChild(xc);
+							Element yc = results.createElement("y");
+							yc.setAttribute("value", y);
+							parameters.appendChild(yc);
+							Element r = results.createElement("radius");
+							r.setAttribute("value", radius);
+							parameters.appendChild(r);
+							Element c = results.createElement("color");
+							c.setAttribute("value", color);
+							parameters.appendChild(c);
+						}
+						else {
+							// append output to results
+							Element success = results.createElement("success");
+							root.appendChild(success);
+							Element comm = results.createElement("command");
+							success.appendChild(comm);
+							comm.setAttribute("name", command.getNodeName());
+							Element parameters = results.createElement("parameters");
+							success.appendChild(parameters);
+							Element n = results.createElement("name");
+							n.setAttribute("value", name);
+							parameters.appendChild(n);
+							Element xc = results.createElement("x");
+							xc.setAttribute("value", x);
+							parameters.appendChild(xc);
+							Element yc = results.createElement("y");
+							yc.setAttribute("value", y);
+							parameters.appendChild(yc);
+							Element r = results.createElement("radius");
+							r.setAttribute("value", radius);
+							parameters.appendChild(r);
+							Element c = results.createElement("color");
+							c.setAttribute("value", color);
+							parameters.appendChild(c);
+							Element output = results.createElement("output");
+							success.appendChild(output);
+							City added = new City(name, String.valueOf(Integer.parseInt(x)), String.valueOf(Integer.parseInt(y)), color, String.valueOf(Integer.parseInt(radius)));
+							clist.add(added);
+							names.insert(added);
+							//TODO: add to sg tree
+						}
+					}
+					else if (command.getNodeName().equals("listCities")) {
+						// parse input command
+						String sortBy = command.getAttribute("sortBy");
+						
+						if (clist.size() == 0) {
+							Element error = results.createElement("error");
+							root.appendChild(error);
+							error.setAttribute("type", "noCitiesToList");
+							Element comm = results.createElement("command");
+							comm.setAttribute("name", command.getNodeName());
+							error.appendChild(comm);
+							Element parameters = results.createElement("parameters");
+							error.appendChild(parameters);
+							Element sB = results.createElement("sortBy");
+							sB.setAttribute("value", sortBy);
+							parameters.appendChild(sB);
+						}
+						else {
+							// append output to results
+							Element success = results.createElement("success");
+							root.appendChild(success);
+							Element comm = results.createElement("command");
+							success.appendChild(comm);
+							comm.setAttribute("name", command.getNodeName());
+							Element parameters = results.createElement("parameters");
+							success.appendChild(parameters);
+							Element sB = results.createElement("sortBy");
+							sB.setAttribute("value", sortBy);
+							parameters.appendChild(sB);
+							Element output = results.createElement("output");
+							success.appendChild(output);
+							Element cityList = results.createElement("cityList");
+							
+							// sortBy for all cities
+							ArrayList<City> templist = new ArrayList<City>();
+							if (sortBy.compareTo("name") == 0) {
+								templist = names.inOrderTraversal();
+								clist = templist;
+							}
+							else {
+								clist.sort((a,b) ->{return a.compareToCoords(b);});
+								//TODO: change to sgtree traversal
+							}
+							
+							// add cities to city list
+							for (City city : clist) {
+								Element el = results.createElement("city");
+								el.setAttribute("name", city.name);
+								el.setAttribute("x", city.x);
+								el.setAttribute("y", city.y);
+								el.setAttribute("radius", city.radius);
+								el.setAttribute("color", city.color);
+								cityList.appendChild(el);
+							}
+							
+							output.appendChild(cityList);
+						}
+					}
+					else if (command.getNodeName().equals("deleteCity")) {
+						// parse input command
+						String name = command.getAttribute("name");
+
+						City found = null;
+						for (City cit : clist) {
+							if (cit.name.compareTo(name) == 0) {
+								found = cit;
+								clist.remove(cit);
+								names.delete(cit);
+								//TODO: remove from sgtree
+								break;
+							}
+						}
+						
+						if (found == null) { // city was not found
+							Element error = results.createElement("error");
+							root.appendChild(error);
+							error.setAttribute("type", "cityDoesNotExist");
+							Element comm = results.createElement("command");
+							comm.setAttribute("name", command.getNodeName());
+							error.appendChild(comm);
+							Element parameters = results.createElement("parameters");
+							error.appendChild(parameters);
+							Element sB = results.createElement("name");
+							sB.setAttribute("value", name);
+							parameters.appendChild(sB);
+						}
+						else {
+							// append output to results
+							Element success = results.createElement("success");
+							root.appendChild(success);
+							Element comm = results.createElement("command");
+							success.appendChild(comm);
+							comm.setAttribute("name", command.getNodeName());
+							Element parameters = results.createElement("parameters");
+							success.appendChild(parameters);
+							Element n = results.createElement("name");
+							n.setAttribute("value", name);
+							parameters.appendChild(n);
+							Element output = results.createElement("output");
+							Element el = results.createElement("cityDeleted");
+							el.setAttribute("name", found.name);
+							el.setAttribute("x", found.x);
+							el.setAttribute("y", found.y);
+							el.setAttribute("radius", found.radius);
+							el.setAttribute("color", found.color);
+							output.appendChild(el);
+							success.appendChild(output);
+						}
+						
+					}
+					else if (command.getNodeName().equals("clearAll")) {
+						clist = new ArrayList<City>();
+						names= new BinarySearchTree<City>();
+						//TODO: reset sg tree
+						
+						// append output to results
+						Element success = results.createElement("success");
+						root.appendChild(success);
+						Element comm = results.createElement("command");
+						success.appendChild(comm);
+						comm.setAttribute("name", command.getNodeName());
+						Element parameters = results.createElement("parameters");
+						success.appendChild(parameters);
+						Element output = results.createElement("output");
+						success.appendChild(output);
+					}
+					else if (command.getNodeName().equals("printBinarySearchTree")) {
+						if (names.height() == -1) { // no cities in tree
+							Element error = results.createElement("error");
+							root.appendChild(error);
+							error.setAttribute("type", "mapIsEmpty");
+							Element comm = results.createElement("command");
+							comm.setAttribute("name", command.getNodeName());
+							error.appendChild(comm);
+							Element parameters = results.createElement("parameters");
+							error.appendChild(parameters);
+						}
+						else {
+							// append output to results
+							Element success = results.createElement("success");
+							root.appendChild(success);
+							Element comm = results.createElement("command");
+							success.appendChild(comm);
+							comm.setAttribute("name", command.getNodeName());
+							Element parameters = results.createElement("parameters");
+							success.appendChild(parameters);
+							Element output = results.createElement("output");
+							Element bst = results.createElement("binarysearchtree");
+							names.printing(results, bst);
+							output.appendChild(bst);
+							success.appendChild(output);
+						}
+					}
+					else if (command.getNodeName().equals("printSGTree")) {
+						//TODO
+					}
+				}
+			}
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			Element root = results.createElement("fatalError");
+			results.appendChild(root);
+
+		} finally {
+			try {
+				// print the contents of your results document
+				XmlUtility.print(results);
+			} catch (TransformerException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
